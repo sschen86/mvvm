1b6ff63990185297a7fea31459327863344a4af4
zjfeihu <zjfeihu@126.com>
2019-12-19 20:30:52 +0800

    基本跑通

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: [
+        '@smartx/eslint-config-tentative',
+    ],
+    parser: "babel-eslint",
+    parserOptions: { 
+       
+    },
+    globals: {
+        
+    },
+    rules: {
+        'no-cond-assign': 'off',
+        'no-new-func': 'off',
+    },
+}
---
+++ develop/src/compiler-test.js
@@ -0,0 +1,17 @@
+// html编译器
+import compiler from '../../lib/view/compiler'
+
+const ast = compiler(`
+    <div>
+        <h2 :nx="a.b">{{title}}</h2>
+        <ul class="list" >
+            <li :each="items as item"><text :value="item.name"></li>
+        </ul>
+    </div>
+
+    <!-- xxx -->
+
+    <div>xxx</div>
+`)
+
+console.info({ ast })
